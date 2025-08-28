037719699a8ba2a7370786b563d3d2de1ad53aa0
William Estoque <william.estoque@gmail.com>
2025-08-29 00:18:31 +0900

    Add basic user create and list in userservice.

--- src/frontend/routes/users.js
+++ src/frontend/routes/users.js
@@ -29,8 +29,31 @@ router.get('/', function(req, res, next) {
     console.log('Got response for listUsers gRPC call: ', response);
     if (err) {
       res.status(500).send('Internal Server Error');
+      return;
     }
-    res.json(response.users);
+    res.render('users', { title: 'Users', users: response.users });
+  });
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create-user', { title: 'Create New User' });
+});
+
+router.post('/create', function(req, res, next) {
+  const user = {
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    email: req.body.email
+  };
+
+  console.log('Make createUser gRPC call to UserService');
+  client.createUser(user, (err, response) => {
+    if (err) {
+      console.error('Error creating user:', err);
+      res.status(500).send('Error creating user');
+      return;
+    }
+    res.redirect('/users');
   });
 });
 
